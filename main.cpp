57f3eb72a105c78741cebd32276ab87b3dc9bb8a
James <johnson.8246@coeit.osu.edu>
2020-02-12 14:20:50 -0500

    Copied Brendan's base code from Exploration 1

--- main.cpp
+++ main.cpp
@@ -1,21 +1,122 @@
 #include <FEHLCD.h>
 #include <FEHUtility.h>
+#include <FEHMotor.h>
+#include <FEHIO.h>
+#include <FEHServo.h>
 
-int main(void)
-{
-    float x,y;
+/*
+ Completes a back right turn
+*/
+void backRightTurn(FEHMotor left,DigitalInputPin backLeft,
+                   FEHMotor right, DigitalInputPin backRight) {
+    /*
+     * Back robot up slightly and turn
+    */
+    while(true) {
+        right.SetPercent(-15);
+        left.SetPercent(-15);
+        Sleep(0.2);
+        break;
+    }
+    left.SetPercent(0);
+    right.SetPercent(0);
 
-    LCD.Clear(FEHLCD::Black);
-    LCD.SetFontColor(FEHLCD::White);
+    /*
+     * Wait for robot to stop
+    */
+     Sleep(2.0);
 
-    while(true)
-    {
-        if(LCD.Touch(&x,&y))
-        {
-            LCD.WriteLine("Hello World!");
-            Sleep(100);
-        }
+     /*
+      * Backward right turn
+    */
+    while(backRight.Value()) {
+        right.SetPercent(-25);
+    }
+    right.SetPercent(0);
+    right.SetPercent(0);
+
+    while(backLeft.Value()) {
+        left.SetPercent(-25);
+    }
+    left.SetPercent(0);
+    Sleep(1.0);
+}
+
+/*
+ * Completes a back left turn
+ */
+void backLeftTurn(FEHMotor left, DigitalInputPin backLeft,
+                  FEHMotor right, DigitalInputPin backRight) {
+    /*Slight back up */
+    while(true) {
+        right.SetPercent(-15);
+        left.SetPercent(-15);
+        Sleep(0.2);
+        break;
     }
 
-    return 0;
+    /*
+     * Left motor until it hits the wall
+    */
+    left.SetPercent(0);
+    right.SetPercent(0);
+    while(backLeft.Value()) {
+        left.SetPercent(-25);
+    }
+    left.SetPercent(0);
+    while(backRight.Value()) {
+        right.SetPercent(-25);
+    }
+    right.SetPercent(0);
+    Sleep(1.0);
+
+}
+
+/*
+ * Drive the robot forward until it hits the wall
+ */
+void fullForward(FEHMotor left, DigitalInputPin frontLeft,
+                 FEHMotor right, DigitalInputPin frontRight) {
+    /*
+     * Drive forward until both front switches are hit
+     */
+    while(frontRight.Value() && frontLeft.Value()) {
+        right.SetPercent(25);
+        left.SetPercent(25);
+    }
+    left.SetPercent(0);
+    right.SetPercent(0);
+
+    /*
+     * Wait for robot to stop
+     */
+     Sleep(1.0);
+}
+
+int main(void) {
+    /*Declare microswitches */
+    DigitalInputPin frontRight(FEHIO::P0_1);
+    DigitalInputPin backRight(FEHIO::P0_0);
+    DigitalInputPin frontLeft(FEHIO::P3_6);
+    DigitalInputPin backLeft(FEHIO::P3_7);
+
+    /* Declare microswitches */
+    FEHMotor right(FEHMotor::Motor1, 9.0);
+    FEHMotor left(FEHMotor::Motor0, 9.0);
+    float x, y;
+
+    LCD.Clear();
+    LCD.Write("Trevor smells like socks");
+    /* Wait for touch*/
+    while(!LCD.Touch(&x,&y));
+    /* Wait for release*/
+    while(LCD.Touch(&x,&y));
+
+     fullForward(left, frontLeft, right, frontRight);
+
+     backRightTurn(left, backLeft,  right, backRight);
+
+     fullForward(left, frontLeft, right, frontRight);
+
+     backLeftTurn(left, backLeft, right, backRight);
 }
